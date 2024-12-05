f0d64bb68db7b9ced9fc83c5d8bf81e5e88aa32d
Angelina0905 <yudriangell@gmail>
2024-12-05 18:06:50 +0700

    versi kedua

--- tugas1.py
+++ tugas1.py
@@ -80,3 +80,5 @@ for lokasi, data in data_panen.items():
         print(f"Lokasi {nama_lokasi} memerlukan perhatian khusus.")
     else:
         print(f"Lokasi {nama_lokasi} dalam kondisi baik.")
+
+print("Angelina")
